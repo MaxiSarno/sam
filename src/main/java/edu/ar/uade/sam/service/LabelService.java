5ba4105281bebec99f7ebfea9951926e9321a514
Maxi Sarno <maxisar@gmail.com>
2016-07-04 00:58:57 -0300

    design service

--- src/main/java/edu/ar/uade/sam/service/LabelService.java
+++ src/main/java/edu/ar/uade/sam/service/LabelService.java
@@ -6,6 +6,13 @@ import edu.ar.uade.sam.model.Label;
 
 public interface LabelService {
 	
+	/**
+	 * Create n labels associated to n descriptions for a specific test.  
+	 * 
+	 * @param testId
+	 * @param descriptions
+	 * @return
+	 */
 	public List<Label> createLabels(Integer testId, List<String> descriptions);
 
 }
