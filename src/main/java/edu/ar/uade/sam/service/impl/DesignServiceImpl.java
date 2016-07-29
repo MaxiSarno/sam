374b0e65b306cb4cccfb02204501a3f3d91317f7
Maxi Sarno <maxisar@gmail.com>
2016-07-29 00:34:05 -0300

    diseño de las pruebas

--- src/main/java/edu/ar/uade/sam/service/impl/DesignServiceImpl.java
+++ src/main/java/edu/ar/uade/sam/service/impl/DesignServiceImpl.java
@@ -1,20 +1,32 @@
 package edu.ar.uade.sam.service.impl;
 
+import java.util.Arrays;
 import java.util.List;
 
+import javax.inject.Inject;
+
+import com.google.common.annotations.VisibleForTesting;
+
+import edu.ar.uade.sam.model.Label;
 import edu.ar.uade.sam.model.TestDesign;
 import edu.ar.uade.sam.service.DesignService;
+import edu.ar.uade.sam.service.LabelService;
 
 public class DesignServiceImpl implements DesignService {
 	
-//	@Inject
-//	LabelService labelService;
+	@Inject
+	private LabelService labelService;
 
 	public TestDesign createTestDesign(Integer testId, int personnel, List<String> samples) {
-//		List<String> descriptions = Arrays.asList("Tang", "Clight", "BC");
-//		List<Label> labels = labelService.createLabels(testId , descriptions);
-//		System.out.println(labels);
+		List<String> descriptions = Arrays.asList("Tang", "Clight", "BC");
+		List<Label> labels = labelService.createLabels(testId , descriptions);
+		System.out.println(labels);
 		return null;
+	}
+
+	@VisibleForTesting
+	public void setLabelService(LabelService labelService) {
+		this.labelService = labelService;
 	}
 
 }
