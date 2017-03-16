151b90fcb7f57e9de81d1f1e4fea5d8b61ce549d
Maxi Sarno <maximiliano.sarno@avantrip.com>
2017-03-16 00:04:12 -0300

    results controller, reestructuracion de controllers, y la micheti no haria paro si cobrara 9 lucas (cobra 160)

---
+++ src/main/java/edu/uade/sam/controller/ResultsController.java
@@ -0,0 +1,24 @@
+package edu.uade.sam.controller;
+
+import javax.inject.Inject;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.uade.sam.service.ResultsService;
+
+@RestController
+@RequestMapping("/evaluation/{id}/results")
+public class ResultsController {
+	
+	@Inject
+	private ResultsService resultsService;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public void get(@PathVariable(value="{id}") Integer id) {
+		resultsService.getResult(id);
+	}
+
+}
