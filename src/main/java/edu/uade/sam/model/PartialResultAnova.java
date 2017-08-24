07960bff6a0b889da5bb6f6e0a0c1bcdc24bd3f3
Maxi Sarno <maximiliano.sarno@avantrip.com>
2017-08-24 00:47:34 -0300

    entities para partial results hijos

--- src/main/java/edu/uade/sam/model/PartialResultAnova.java
+++ src/main/java/edu/uade/sam/model/PartialResultAnova.java
@@ -2,17 +2,24 @@ package edu.uade.sam.model;
 
 import java.util.List;
 
+import javax.persistence.Entity;
+
 /**
  * @author msarno
  *
  */
+@Entity
 public class PartialResultAnova extends PartialResult {
 
-	private final double fValue;
-	private final double fCritValue;
-	private final double pValue;
-	
-	public PartialResultAnova(String attributeName, boolean areDifferent, List<ResultSummary> summaries, double fValue, double fCritValue, double pValue) {
+	private double fValue;
+	private double fCritValue;
+	private double pValue;
+
+	public PartialResultAnova() {
+	}
+
+	public PartialResultAnova(String attributeName, boolean areDifferent, List<ResultSummary> summaries, double fValue,
+			double fCritValue, double pValue) {
 		super(attributeName, Distribution.ANOVA, areDifferent, summaries);
 		this.fValue = fValue;
 		this.fCritValue = fCritValue;
@@ -23,12 +30,24 @@ public class PartialResultAnova extends PartialResult {
 		return fValue;
 	}
 
+	public void setfValue(double fValue) {
+		this.fValue = fValue;
+	}
+
 	public double getfCritValue() {
 		return fCritValue;
 	}
 
+	public void setfCritValue(double fCritValue) {
+		this.fCritValue = fCritValue;
+	}
+
 	public double getpValue() {
 		return pValue;
+	}
+
+	public void setpValue(double pValue) {
+		this.pValue = pValue;
 	}
 
 }
--- src/main/java/edu/uade/sam/model/PartialResultStudent.java
+++ src/main/java/edu/uade/sam/model/PartialResultStudent.java
@@ -2,12 +2,18 @@ package edu.uade.sam.model;
 
 import java.util.List;
 
+import javax.persistence.Entity;
+
 /**
  * @author msarno
  *
  */
+@Entity
 public class PartialResultStudent extends PartialResult {
-	
+
+	public PartialResultStudent() {
+	}
+
 	public PartialResultStudent(String attributeName, boolean areDifferent, List<ResultSummary> summaries) {
 		super(attributeName, Distribution.STUDENT_T, areDifferent, summaries);
 	}
