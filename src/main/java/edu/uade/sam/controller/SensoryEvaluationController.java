d5b59223d52ee648242959ac5c6e5280939b588a
Maxi Sarno <maximiliano.sarno@avantrip.com>
2017-08-27 19:18:47 -0300

    validacion para el tipo de evaluacion sensorial

--- src/main/java/edu/uade/sam/controller/SensoryEvaluationController.java
+++ src/main/java/edu/uade/sam/controller/SensoryEvaluationController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,9 +23,16 @@ public class SensoryEvaluationController {
 	private SensoryEvaluationService evaluationService;
 
 	@RequestMapping(method=RequestMethod.POST)
-	public Long save(@RequestParam(value="name", required=true) String name, 
+	public ResponseEntity<Long> save(@RequestParam(value="name", required=true) String name, 
 			@RequestParam(value="type", required=true)String type) {
-		return evaluationService.save(name, SensoryEvaluationType.fromString(type));
+		
+		SensoryEvaluationType samType = SensoryEvaluationType.fromString(type);
+		
+		if (samType==null) {
+			return ResponseEntity.badRequest().build();
+		}
+		
+		return ResponseEntity.ok().body(evaluationService.save(name, samType));
 	}
 	
 	@RequestMapping(method=RequestMethod.GET)
