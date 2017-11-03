7ccd556dd86b8c21ffbdbf927eb9f034603749e3
Maxi Sarno <maximiliano.sarno@avantrip.com>
2017-11-03 17:34:56 -0300

    cors allow sam-ui

--- src/main/java/edu/uade/sam/SamApplication.java
+++ src/main/java/edu/uade/sam/SamApplication.java
@@ -2,9 +2,13 @@ package edu.uade.sam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @author msarno
@@ -20,4 +24,14 @@ public class SamApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SamApplication.class, args);
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 }
